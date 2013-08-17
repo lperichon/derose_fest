3418528aa4fe35753ec933682311c25fcb1e6a92
Dwayne Macgowan <dwaynemac@gmail.com>
2013-08-17 16:28:11 -0300

    mini-refactor of feed.js for DRY

--- data/feed.js
+++ data/feed.js
@@ -1,3 +1,23 @@
+var olimpic = "Salón Olímpic";
+var paris =   "Salón Paris";
+var viernes = "Viernes, 09/21/2013";
+var sabado  = "Sábado, 09/22/2013";
+var domingo = "Domingo, 09/23/2013";
+
+var joris = {
+    "icon": null,
+    "first_name": "Joris",
+    "photo": "resources/images/joris.jpg",
+    "position": "Presidente de la ArchiFederación del Método DeRose de Santa Catarina, Brasil",
+    "name": "Prof. Joris Marengo",
+    "affiliation": "",
+    "url": "http://www.novoexecutivo.com.br/",
+    "bio": "<p>Autor del libro Yôga Antigo para iniciantes.</p>",
+    "id": 1,
+    "twitter": "",
+    "last_name": "Marengo"
+};
+
 Ext.data.JsonP.feedCb({
     "proposals": [
         {
@@ -17,67 +37,43 @@ Ext.data.JsonP.feedCb({
             "id": 1,
             "time": "2013-09-21T13:00:00",
             "pretty_time": "1:00pm",
-            "day": "Viernes, 09/21/2013"
+            "day": viernes
         },
         {
             "topics": [
                 "Conversation",
                 "Live Stream"
             ],
-            "room": "Salón Olimpic",
+            "room": olimpic,
             "end_time": "2013-09-22T10:00:00",
             "description": "",
             "title": "Práctica ortodoxa con énfasis en reeducación comportamental",
             "url": "",
             "date": "09/22/2013",
-            "speakers": [
-                {
-                    "icon": null,
-                    "first_name": "Joris",
-                    "photo": "http://assets.en.oreilly.com/1/eventprovider/1/_@user_6262.jpg",
-                    "position": "Presidente de la Federación del Método DeRose de Santa Catarina, Brasil",
-                    "name": "Prof. Joris Marengo",
-                    "id": 1,
-                    "last_name": "Marengo"
-                }
-            ],
+            "speakers": [joris],
             "proposal_type": "Práctica",
             "id": 2,
             "time": "2013-09-22T10:00:00",
             "pretty_time": "10:00am",
-            "day": "Sabado, 09/22/2013"
+            "day": sabado
         },
         {
             "topics": [
                 "Conversation",
                 "Live Stream"
             ],
-            "room": "Salón Paris",
+            "room": paris,
             "end_time": "2013-09-23T11:45:00",
             "description": "",
             "title": "Práctica heterodoxa con énfasis en cambio de karma, sólo para instructores",
             "url": "",
             "date": "09/23/2013",
-            "speakers": [
-                {
-                    "icon": null,
-                    "first_name": "Joris",
-                    "photo": "resources/images/joris.jpg",
-                    "position": "Presidente de la Federación del Método DeRose de Santa Catarina, Brasil",
-                    "name": "Prof. Joris Marengo",
-                    "affiliation": "",
-                    "url": "http://www.novoexecutivo.com.br/",
-                    "bio": "<p>Autor del libro Yôga Antigo para iniciantes.</p>",
-                    "id": 1,
-                    "twitter": "",
-                    "last_name": "Marengo"
-                }
-            ],
+            "speakers": [joris],
             "proposal_type": "Práctica",
             "id": 3,
             "time": "2013-09-23T11:45:00",
             "pretty_time": "11:45am",
-            "day": "Domingo, 09/23/2013"
+            "day": domingo
         }
     ]
 });
