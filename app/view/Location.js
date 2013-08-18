cbf18757a1c7d2cdacc3393811bcd4eb9bb46104
Luis Perichon <luisperichon@gmail.com>
2013-08-18 16:34:03 -0300

    [#55359566] Use google maps to show map and markers

--- app/view/Location.js
+++ app/view/Location.js
@@ -1,54 +1,32 @@
 Ext.define('DeRoseFest.view.Location', {
-
-	extend: 'Ext.Container',
-	requires: 'Ext.BingMap',
-
-	xtype: 'location',
-
-	config: {
-
-		title: 'Mapa',
+    extend: 'Ext.Map',
+    config: {
+	    title: 'Mapa',
 		iconCls: 'locate',
-
-		layout: 'fit',
-
+ 
+    	layout: 'fit',
 		items: [
 			{
 				docked: 'top',
 				xtype: 'toolbar',
 				title: 'Location'
 			}
-		]
-	},
-
-	initialize: function() {
-		var position = new Microsoft.Maps.Location(DeRoseFest.app.mapCenter[0], DeRoseFest.app.mapCenter[1]),
-  			map;
-
-		this.callParent();
-
-        var mapCallback = function(){
-            map = map.getMap();
-            var infobox = new Microsoft.Maps.Infobox(position, {
-                description: DeRoseFest.app.mapText, visible: true, offset: new Microsoft.Maps.Point(0, 10), height: 100,width:180
-            });
-            map.entities.clear();
-            var pushpin= new Microsoft.Maps.Pushpin(map.getCenter(), null);
-                pushpinClick= Microsoft.Maps.Events.addHandler(pushpin, 'click', function() {
-                map.entities.push(infobox);
-                infobox.setOptions({visible:true});
-            });
-            map.entities.push(pushpin);
+		], 
+	    mapOptions: {
+		    center: new google.maps.LatLng(DeRoseFest.app.mapCenter[0], DeRoseFest.app.mapCenter[1]),
+		    mapTypeId: google.maps.MapTypeId.ROADMAP,
+		    zoom: 17
+	    },
+        listeners: {
+            maprender : function(comp, map) {
+                new google.maps.Marker({
+                    map       : this.getMap(),
+                    position  : new google.maps.LatLng(DeRoseFest.app.mapCenter[0], DeRoseFest.app.mapCenter[1]),
+                    title     : 'DeRose Festival',
+                    animation : google.maps.Animation.DROP,
+                    draggable : false
+                });
+            }
         }
-
-		map = this.add({
-			xtype: 'map',
-			mapOptions: {
-				center: position,
-				zoom: 17,
-        mapTypeId: Microsoft.Maps.MapTypeId.ROADMAP,
-        callback: mapCallback
-			}
-	    });
 	}
-});
+}); 
