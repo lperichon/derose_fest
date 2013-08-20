22406b3b9a5a5959038f32b2b3679a3caeb970b6
Luis Perichon <luisperichon@gmail.com>
2013-08-20 15:40:05 -0300

    [#55347758] Icons and splash screens

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ Ext.require('DeRoseFest.util.Proxy');
 Ext.application({
     // Change the values below to re-configure the app for a different conference.
 
-    title:   'DeRose Festival Buenos Aires 2013',
+    title:   'X DeRose Festival Buenos Aires',
     dataUrl: '',
 
     twitterSearch: '#DeRoseFestivalBA',
@@ -39,24 +39,6 @@ Ext.application({
     // App namespace
 
     name: 'DeRoseFest',
-
-    startupImage: {
-        '320x460': 'resources/startup/Default.jpg', // Non-retina iPhone, iPod touch, and all Android devices
-        '640x920': 'resources/startup/640x920.png', // Retina iPhone and iPod touch
-        '640x1096': 'resources/startup/640x1096.png', // iPhone 5 and iPod touch (fifth generation)
-        '768x1004': 'resources/startup/768x1004.png', //  Non-retina iPad (first and second generation) in portrait orientation
-        '748x1024': 'resources/startup/748x1024.png', //  Non-retina iPad (first and second generation) in landscape orientation
-        '1536x2008': 'resources/startup/1536x2008.png', // : Retina iPad (third generation) in portrait orientation
-        '1496x2048': 'resources/startup/1496x2048.png' // : Retina iPad (third generation) in landscape orientation
-    },
-
-    isIconPrecomposed: false,
-    icon: {
-        57: 'resources/icons/icon.png',
-        72: 'resources/icons/icon@72.png',
-        114: 'resources/icons/icon@2x.png',
-        144: 'resources/icons/icon@72.png'
-    },
 
     // Dependencies
 
@@ -106,25 +88,10 @@ Ext.application({
     ],
 
     launch: function() {
-
-//        Ext.Viewport.setMasked({ xtype: 'loadmask' });
-
         DeRoseFest.util.Proxy.process('data/feed.js', function() {
             Ext.Viewport.add({ xtype: 'main' });
             Ext.Viewport.setMasked(false);
         });
-
-        // Splash Screen
-        var splash = Ext.create('Ext.Panel', {
-            fullscreen: true,
-            id: 'splashScreen'
-        });
-        splash.show();
-        Ext.defer(function() { splash.destroy(); }, 2500);
-
-        // setInterval(function(){
-        //     Ext.DomQuery.select('link')[0].href = "resources/css/oreilly.css?" + Math.ceil(Math.random() * 100000000)
-        // }, 1000);
     }
 
 });
