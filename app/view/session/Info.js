28057b4bf1e7a2dc84f1f090d506203ba7b90c69
Luis Perichon <luisperichon@gmail.com>
2013-08-22 10:39:33 -0300

    [#55347450] Share links

--- app/view/session/Info.js
+++ app/view/session/Info.js
@@ -11,6 +11,9 @@ Ext.define('DeRoseFest.view.session.Info', {
 			'<h3>{title} <small>{room}</small></h3>',
 			'<h4>{[Ext.Date.format(values.time, "l")]} a las {[this.formatTime(values.time)]}</h4>',
 			'<p>{description}</p>',
+			'<a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fderosefestival.com.ar" target="_blank">Share on Facebook</a>',
+			'<a href="http://twitter.com/home?status=Voy%20a%20participar%20de%20XXX%20en%20el%20DeroseFestival%20" target="_blank">Share on Twitter</a>',
+			'<a href="https://plus.google.com/share?url=http://www.derosefestival.com.ar">Share on Google+</a>',
 			{
 				formatTime: function(time) {
 					return Ext.Date.format(time, 'g:ia')
