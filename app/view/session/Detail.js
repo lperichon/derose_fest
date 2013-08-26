200d44630cee5d4a144b507cdbbffecf39bda24e
Luis Perichon <luisperichon@gmail.com>
2013-08-25 22:01:04 -0300

    style up share links

--- app/view/session/Detail.js
+++ app/view/session/Detail.js
@@ -1,3 +1,15 @@
+var facebook_handler = function (btn, evt) {
+    window.location = 'https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fderosefestival.com.ar';
+}
+
+var twitter_handler = function (btn, evt) {
+    window.location = 'http://twitter.com/home?status=Voy%20a%20participar%20de%20XXX%20en%20el%20DeroseFestival%20';
+}
+
+var plus_handler = function (btn, evt) {
+    window.location = 'https://plus.google.com/share?url=http://www.derosefestival.com.ar';
+}
+
 Ext.define('DeRoseFest.view.session.Detail', {
 
 	extend: 'Ext.Container',
@@ -11,7 +23,6 @@ Ext.define('DeRoseFest.view.session.Detail', {
 		listeners: {
             painted : function() { document.getElementsByClassName('x-toolbar-dark')[0].style.height='46px'; }
         },		
-
 		items: [
 			{
                 flex: 1,
@@ -19,6 +30,44 @@ Ext.define('DeRoseFest.view.session.Detail', {
                 scrollable: 'vertical',
 				xtype: 'sessionInfo'
 			},
+            {
+                xtype: 'component',
+                cls: 'dark',
+                html: 'Compartí en la web'
+            },
+            {
+                xtype: 'toolbar',
+                ui: 'grey',
+                items: [
+
+                    {
+                        xtype: 'button',
+                        itemId: 'facebook_share',
+                        cls: 'facebook_share',
+                        margin: '10',
+                        ui: 'action',
+                        text: 'Facebook',
+                        handler: facebook_handler
+                    },
+                    {
+                        xtype: 'button',
+                        itemId: 'twitter_share',
+						cls: 'twitter_share',
+                        margin: '10',
+                        ui: 'action',
+                        text: 'Twitter'
+                    },
+                    {
+                        xtype: 'button',
+                        itemId: 'plus_share',
+                        cls: 'plus_share',
+                        margin: '10',
+                        ui: 'decline',
+                        text: 'Google+'
+                    }
+                ]
+            },
+
             {
                 xtype: 'component',
                 cls: 'dark',
